7f2bb52ca463e61c31fb46f2af573c2d15b48d51
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-02-16 21:14:08 +0100

    Add State and button

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: "Alice", age: 20 },
+            { name: "Bob", age: 21 },
+            { name: "Charlie", age: 19 }
+        ]
+    }
+
+    switchNameHandler = () => {
+        
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>This is a sample react app</h1>
                 <p>and it works</p>
-
-                <Person name="Alice" age="20" />
-                <Person name="Bob" age="21">Hobby: Playing Bass</Person>
-                <Person name="Charlie" age="19" />
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobby: Playing Bass</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
             </div>
         );
     }
